6b0d49cbe95fa2cafa45f9dd54f24c3dee46e8e1
OpenSpace <67124471+Mensindex@users.noreply.github.com>
2021-06-13 22:58:52 +0300

    Есть свайп, но иногда некоторые итемы не свайпаются обратно автоматически

--- app/src/main/java/com/example/gambittest/DishModel.java
+++ app/src/main/java/com/example/gambittest/DishModel.java
@@ -7,6 +7,7 @@ public class DishModel {
     private int price;
     private String description;
     private int count;
+    private boolean isFavorite;
 
 
     public String getId() {
@@ -37,12 +38,21 @@ public class DishModel {
         this.count = count;
     }
 
-    public DishModel(String id, String name, String image, int price, String description, int count) {
+    public boolean getFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public DishModel(String id, String name, String image, int price, String description, int count, boolean isFavorite) {
         this.id = id;
         this.name = name;
         this.image = image;
         this.price = price;
         this.description = description;
         this.count = count;
+        this.isFavorite = isFavorite;
     }
 }
--- app/src/main/java/com/example/gambittest/DishModelAdapter.java
+++ app/src/main/java/com/example/gambittest/DishModelAdapter.java
@@ -7,20 +7,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
 
 import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class DishModelAdapter extends RecyclerView.Adapter<DishModelAdapter.ViewHolder> {
+public class DishModelAdapter extends RecyclerSwipeAdapter<DishModelAdapter.ViewHolder> {
     private final List<DishModel> dishes;
     private Context context;
     private static SharedPreferences sPref;
+    private static SharedPreferences sPref2;
 
     public DishModelAdapter(List<DishModel> dishes, Context context) {
         this.dishes = dishes;
@@ -44,25 +48,51 @@ public class DishModelAdapter extends RecyclerView.Adapter<DishModelAdapter.View
         return dishes.size();
     }
 
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.mySwipe;
+    }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView imageViewDish, imageViewMinus, imageViewPlus, imageViewCart;
+        private ImageView imageViewDish, imageViewMinus, imageViewPlus, imageViewCart, imageViewFavOff, imageViewFavOn;
         private TextView dishName, dishPrice, dishCount;
+        private SwipeLayout swipeLayout;
 
         ViewHolder(View view) {
             super(view);
+            swipeLayout = view.findViewById(R.id.mySwipe);
             imageViewDish = view.findViewById(R.id.imageViewDish);
             imageViewCart = view.findViewById(R.id.imageViewCart);
             imageViewMinus = view.findViewById(R.id.imageViewMinus);
             imageViewPlus = view.findViewById(R.id.imageViewPlus);
+            imageViewFavOff = view.findViewById(R.id.imageViewFavOff);
+            imageViewFavOn = view.findViewById(R.id.imageViewFavOn);
             dishName = view.findViewById(R.id.textViewDishName);
             dishPrice = view.findViewById(R.id.textViewPrice);
             dishCount = view.findViewById(R.id.textViewCount);
         }
 
+
         public void bind(DishModel dishModel) {
 
             dishModel.setCount(getCountById(dishModel.getId()));
+
+            dishModel.setFavorite(getFavById(dishModel.getId()));
+
+
+            swipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
+            swipeLayout.addDrag(SwipeLayout.DragEdge.Right, swipeLayout.findViewById(R.id.bottom_wrapper));
+            swipeLayout.setLeftSwipeEnabled(false);
+
+
+            if (dishModel.getFavorite() != true) {
+                imageViewFavOff.setVisibility(View.INVISIBLE);
+                imageViewFavOn.setVisibility(View.VISIBLE);
+            } else {
+                imageViewFavOff.setVisibility(View.VISIBLE);
+                imageViewFavOn.setVisibility(View.INVISIBLE);
+            }
 
             if (dishModel.getCount() > 0) {
                 dishCount.setVisibility(View.VISIBLE);
@@ -77,32 +107,113 @@ public class DishModelAdapter extends RecyclerView.Adapter<DishModelAdapter.View
                 imageViewCart.setVisibility(View.VISIBLE);
             }
 
+            //Добавляю слушатель для свайпа и реализовываю все его методы (В итоге ни один из методов на подошел)
+            /* swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+                @Override
+                public void onStartOpen(SwipeLayout layout) {
+
+                }
+
+                @Override
+                public void onOpen(SwipeLayout layout) {
+
+                }
+
+                @Override
+                public void onStartClose(SwipeLayout layout) {
+                    //Toast.makeText(context, "Закрыл свайп", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onClose(SwipeLayout layout) {
+
+                }
+
+                @Override
+                public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+
+
+                }
+
+                @Override
+                public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                    //when user's hand released.
+
+                }
+
+            }); */
+
+
+            swipeLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if ((swipeLayout.getOpenStatus() == SwipeLayout.Status.Open)) {
+                        //Start your activity
+                        if (dishModel.getFavorite() == false) {
+                            dishModel.setFavorite(true);
+                            saveFavById(dishModel.getId(), dishModel.getFavorite());
+                            Toast.makeText(context, "Добавлено в избранное", Toast.LENGTH_SHORT).show();
+                        } else {
+                            dishModel.setFavorite(false);
+                            saveFavById(dishModel.getId(), dishModel.getFavorite());
+                            Toast.makeText(context, "Удалено из избранного", Toast.LENGTH_SHORT).show();
+                        }
+
+                        notifyItemChanged(getAdapterPosition(), new Object());
+                    }
+
+                }
+            });
+
+
             Glide.with(itemView.getContext())
-                    .load(dishModel.getImage())
-                    .into(imageViewDish);
+                    .
+
+                            load(dishModel.getImage())
+                    .
+
+                            into(imageViewDish);
 
             dishName.setText(dishModel.getName());
             dishPrice.setText(String.valueOf(dishModel.getPrice()));
 
-            imageViewCart.setOnClickListener(v -> {
+            imageViewCart.setOnClickListener(v ->
+            {
                 dishModel.setCount(dishModel.getCount() + 1);
                 saveCountById(dishModel.getId(), dishModel.getCount());
                 notifyItemChanged(getAdapterPosition(), new Object());
             });
 
-            imageViewPlus.setOnClickListener(v -> {
+            imageViewPlus.setOnClickListener(v ->
+            {
                 dishModel.setCount(dishModel.getCount() + 1);
                 saveCountById(dishModel.getId(), dishModel.getCount());
                 notifyItemChanged(getAdapterPosition(), new Object());
             });
 
-            imageViewMinus.setOnClickListener(v -> {
+            imageViewMinus.setOnClickListener(v ->
+            {
                 if (dishModel.getCount() > 0) {
                     dishModel.setCount(dishModel.getCount() - 1);
                     saveCountById(dishModel.getId(), dishModel.getCount());
                     notifyItemChanged(getAdapterPosition(), new Object());
                 }
             });
+        }
+
+
+        private boolean getFavById(String id) {
+            sPref2 = context.getSharedPreferences("fav", MODE_PRIVATE);
+            return sPref2.getBoolean(id, false);
+        }
+
+        void saveFavById(String id, boolean isFavorite) {
+            sPref2 = context.getSharedPreferences("fav", MODE_PRIVATE);
+            sPref2.edit()
+                    .putBoolean(id, isFavorite)
+                    .apply();
         }
 
         private int getCountById(String id) {
